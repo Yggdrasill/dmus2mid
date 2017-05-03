46d984c9b06f1726c5f4585879be058076c9b759
Yggdrasill <yggdrasill@shitpost.is>
2017-05-03 14:07:48 +0200

    Basic reading of the MUS header metadata

--- dmus2mid.c
+++ dmus2mid.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -18,6 +19,11 @@ int main(int argc, char **argv)
 
   size_t size;
 
+  int cmp;
+
+  uint16_t mus_len;
+  uint16_t mus_off;
+
   if(argc < 2) {
     printf("Too few arguments\n");
     exit(EXIT_FAILURE);
@@ -30,7 +36,13 @@ int main(int argc, char **argv)
 
   fread(buffer, sizeof(*buffer), size, mus);
 
-  printf("%d\n", strncmp(MUS_HEADER_MAGIC, buffer, strlen(MUS_HEADER_MAGIC) ) );
+  cmp = strncmp(MUS_HEADER_MAGIC, buffer, strlen(MUS_HEADER_MAGIC) );
+
+  fseek(mus, 4, SEEK_SET);
+  fread(&mus_len, sizeof(mus_len), 1, mus);
+  fread(&mus_off, sizeof(mus_off), 1, mus);
+
+  printf("%d %d %d\n", cmp, mus_len, mus_off);
 
   fclose(mus);
 
