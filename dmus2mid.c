2e9d2489acfe1f7192ab3e84d88173935ee2ee49
Yggdrasill <yggdrasill@shitpost.is>
2017-09-13 01:36:29 +0200

    Fix conversion of PITCH_BEND data
            It appears that my previous attempt was incorrect after all.

--- dmus2mid.c
+++ dmus2mid.c
@@ -359,8 +359,8 @@ int main(int argc, char **argv)
         }
         break;
       case MUS_PITCH_BEND:
-        args[1] = args[0];
-        args[0] = args[0] << 6;
+        args[1] = args[0] >> 1;
+        args[0] = (args[0] & 1) << 6;
         break;
       case MUS_FINISH:
         event = MUS_UNKNOWN2;
